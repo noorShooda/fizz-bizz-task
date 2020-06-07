a35e6d7b8158f2420aee4a38d5895ad654978767
noorShooda <sebai.noorelhooda@gmail.com>
2020-06-07 14:04:44 +0200

    feat: add routing component to handle the routing .

---
+++ src/Root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import HomePage from './homePage/HomePage';
+import { rootingURL } from './config/rootingURL';
+import { Header } from './header/Header';
+
+export const Root = function(){
+    return(
+        <BrowserRouter>
+            <Header/>
+            <Switch>
+                <Route path={rootingURL.home} component={HomePage} exact/>
+                <Redirect to={rootingURL.home}/>
+            </Switch>
+        </BrowserRouter>
+    );
+}
