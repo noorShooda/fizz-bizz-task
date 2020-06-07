7f0bb984f0b131752c2d2253c82b8145c50833b1
noorShooda <sebai.noorelhooda@gmail.com>
2020-06-07 16:48:33 +0200

    update: update CSS style to adapt business logic.

--- src/homePage/ListDisplayer/ListDisplayer.js
+++ src/homePage/ListDisplayer/ListDisplayer.js
@@ -6,19 +6,60 @@ import { verifyDivisibility } from '../../helpers/helpers';
 
 export const ListDisplayer  = (props) => {
     
-    const displyListOfNumbers = () => {
+    const displyFirstListOfNumbers = () => {
         var list = [];
-        for(var i = 1; i <= 100; i++){
+        for(var i = 1; i <= 25; i++){
             const item = verifyDivisibility(i);
             list.push(item);
         }
         return(list);
     }
-    const list = displyListOfNumbers();
+
+    const displySecondListOfNumbers = () => {
+        var list = [];
+        for(var i = 26; i <= 50; i++){
+            const item = verifyDivisibility(i);
+            list.push(item);
+        }
+        return(list);
+    }
+    
+    const displyThirdListOfNumbers = () => {
+        var list = [];
+        for(var i = 51; i <= 75; i++){
+            const item = verifyDivisibility(i);
+            list.push(item);
+        }
+        return(list);
+    }
+
+    const displyForthListOfNumbers = () => {
+        var list = [];
+        for(var i = 76; i <= 100; i++){
+            const item = verifyDivisibility(i);
+            list.push(item);
+        }
+        return(list);
+    }
+    const firstList = displyFirstListOfNumbers();
+    const secondList = displySecondListOfNumbers();
+    const thirdList = displyThirdListOfNumbers();
+    const forthList = displyForthListOfNumbers();
 
     return(
         <div className="listDisplayerStyle">
-            {list.map(x => <ListItem item={x} />)}
+            <div className="listStyle">
+                {firstList.map(x => <ListItem item={x} />)}
+            </div>
+            <div className="listStyle">
+            {secondList.map(x => <ListItem item={x} />)}
+            </div>
+            <div className="listStyle">   
+                {thirdList.map(x => <ListItem item={x} />)}
+            </div>
+            <div className="listStyle">   
+                {forthList.map(x => <ListItem item={x} />)}
+            </div>
         </div>
     );
 }
