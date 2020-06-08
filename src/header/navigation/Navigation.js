12f6d7422e28ba0ae501c640929497d55831f078
noorShooda <sebai.noorelhooda@gmail.com>
2020-06-09 01:20:04 +0200

    feat: add useEffect hook to handle the local storage dependency side effect.

--- src/header/navigation/Navigation.js
+++ src/header/navigation/Navigation.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { BsFillHouseFill } from 'react-icons/bs';
+import React, { useEffect, useState } from 'react';
+import { RiLoginCircleLine } from 'react-icons/ri';
 import { Link } from 'react-router-dom';
 
 import { rootingURL } from '../../config/rootingURL';
 import './navigation.css';
 
 export const Navigation = ()=>{
+
+    const[isLoggedIn,setIsLoggedIn]= useState(localStorage.getItem('isLoggedIn'));
+
+    useEffect(() => {
+        console.log("use effect");
+        setIsLoggedIn(localStorage.getItem('isLoggedIn'));
+    },[]);
+
     return(
         <ul className="listStyle">
-            <li className="listItemStyle"><BsFillHouseFill/><Link className="headerItemStyle" to={rootingURL.home}>Home</Link></li>
+            { isLoggedIn === "true" ? 
+            <li className="listItemStyle">
+                <Link className="headerItemStyle" to={rootingURL.home}>Home</Link>
+            </li> :
+            <li className="listItemStyle">
+                <RiLoginCircleLine/>
+                <Link className="headerItemStyle" to={rootingURL.login}>Log In</Link>
+            </li>
+            }           
         </ul>
     )
 }
