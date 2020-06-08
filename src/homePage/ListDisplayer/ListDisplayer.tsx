c3b06cb89e3d3955083712e04a8ba4ebc12076a7
noorShooda <sebai.noorelhooda@gmail.com>
2020-06-08 11:31:02 +0200

    refactor: convert component to typescript.

--- src/homePage/ListDisplayer/ListDisplayer.tsx
+++ src/homePage/ListDisplayer/ListDisplayer.tsx
@@ -4,21 +4,21 @@ import './listDisplayer.css';
 import { ListItem } from './ListItem';
 import { verifyDivisibility } from '../../helpers/helpers';
 
-export const ListDisplayer  = () => {
+export const ListDisplayer : React.FunctionComponent  = () => {
     
-    const displyListOfNumbers = ([start,end]) => {
-        var list = [];
-        for(var i = start; i <= end; i++){
-            const item = verifyDivisibility(i);
+    const displyListOfNumbers  = (forStart : number , forEnd : number) => {
+        var list : Array<String> = [];
+        for(var i = forStart; i <= forEnd; i++){
+            const item : String = verifyDivisibility(i);
             list.push(item);
         }
         return(list);
     }
 
-    const firstList = displyListOfNumbers([1,25]);
-    const secondList = displyListOfNumbers([26,50]);
-    const thirdList = displyListOfNumbers([51,75]);
-    const forthList = displyListOfNumbers([76,100]);
+    const firstList : Array<String> = displyListOfNumbers(1,25);
+    const secondList : Array<String> = displyListOfNumbers(26,50);
+    const thirdList : Array<String> = displyListOfNumbers(51,75);
+    const forthList : Array<String> = displyListOfNumbers(76,100);
 
     return(
         <div className="listDisplayerStyle">
